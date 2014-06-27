f6c6b766cbcab1bb2950c879b6e2f1f2e560a3af
Dan Macumber <dan.macumber@gmail.com>
2014-06-26 22:27:45 -0600

    Initial measure

---
+++ ViewModel/measure.rb
@@ -0,0 +1,96 @@
+require 'erb'
+
+#start the measure
+class ViewModel < OpenStudio::Ruleset::ReportingUserScript
+  
+  #define the name that a user will see, this method may be deprecated as
+  #the display name in PAT comes from the name field in measure.xml
+  def name
+    return "ViewModel"
+  end
+  
+  #define the arguments that the user will input
+  def arguments()
+    args = OpenStudio::Ruleset::OSArgumentVector.new
+    
+    return args
+  end #end the arguments method
+
+  #define what happens when the measure is run
+  def run(runner, user_arguments)
+    super(runner, user_arguments)
+    
+    #use the built-in error checking 
+    if not runner.validateUserArguments(arguments(), user_arguments)
+      return false
+    end
+
+    # get the last model and sql file
+    
+    model = runner.lastOpenStudioModel
+    if model.empty?
+      runner.registerError("Cannot find last model.")
+      return false
+    end
+    model = model.get
+    
+    sqlFile = runner.lastEnergyPlusSqlFile
+    if sqlFile.empty?
+      runner.registerError("Cannot find last sql file.")
+      return false
+    end
+    sqlFile = sqlFile.get
+    model.setSqlFile(sqlFile)
+ 
+    # put data into variables, these are available in the local scope binding
+    
+    output =  "Measure Name = " << name << "<br>"
+    output << "Building Name = " << model.getBuilding.name.get << "<br>"                       # optional variable
+    output << "Floor Area = " << model.getBuilding.floorArea.to_s << "<br>"                   # double variable
+    output << "Floor to Floor Height = " << model.getBuilding.nominalFloortoFloorHeight.to_s << " (m)<br>" # double variable
+    output << "Net Site Energy = " << sqlFile.netSiteEnergy.to_s << " (GJ)<br>" # double variable
+    
+    web_asset_path = OpenStudio::getSharedResourcesPath() / OpenStudio::Path.new("web_assets")
+
+    # read in template
+    html_in_path = "#{File.dirname(__FILE__)}/resources/report.html.in"
+    if File.exist?(html_in_path)
+        html_in_path = html_in_path
+    else
+        html_in_path = "#{File.dirname(__FILE__)}/report.html.in"
+    end
+    html_in = ""
+    File.open(html_in_path, 'r') do |file|
+      html_in = file.read
+    end
+
+    # configure template with variable values
+    renderer = ERB.new(html_in)
+    html_out = renderer.result(binding)
+
+    # write html file
+    html_out_path = "./report.html"
+    File.open(html_out_path, 'w') do |file|
+      file << html_out
+      # make sure data is written to the disk one way or the other      
+      begin
+        file.fsync
+      rescue
+        file.flush
+      end
+    end
+
+    #closing the sql file
+    sqlFile.close()
+
+    #reporting final condition
+    runner.registerFinalCondition("Goodbye.")
+    
+    return true
+ 
+  end #end the run method
+
+end #end the measure
+
+#this allows the measure to be use by the application
+ViewModel.new.registerWithApplication
