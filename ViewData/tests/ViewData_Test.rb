af62389b0438754a8085359d2b6f03742bc95b3b
Dan Macumber <dan.macumber@gmail.com>
2015-04-06 22:47:38 -0600

    Works with zone variables too

--- ViewData/tests/ViewData_Test.rb
+++ ViewData/tests/ViewData_Test.rb
@@ -90,6 +90,7 @@ class ViewData_Test < MiniTest::Test
     # create hash of argument values
     args_hash = {}
     args_hash["variable_name"] = 'Surface Outside Face Temperature'
+    #args_hash["variable_name"] = 'Zone Mean Air Temperature'
     args_hash["reporting_frequency"] = 'Hourly'
 
     # populate argument with specified hash value if specified
